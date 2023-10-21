fcd4883a3e43c415a4169c06f224e73ed612895f
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-22 02:39:22 +0800

    Remove wrong db settings

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,11 +2,11 @@ locals {
   allowed_cidrs = split(",", var.allowed_cidrs_string)
 }
 
-resource "random_password" "db_master_password" {
-  length           = var.rds_config.main_user_password_length
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
+# resource "random_password" "db_master_password" {
+#   length           = var.rds_config.main_user_password_length
+#   special          = true
+#   override_special = "!#$%&*()-_=+[]{}<>:?"
+# }
 
 # module "db" {
 #   source = "terraform-aws-modules/rds-aurora/aws"
@@ -75,81 +75,81 @@ resource "random_password" "db_master_password" {
 # }
 
 
-resource "aws_security_group" "db_rds_security_group" {
-  name        = "${var.rds_config.main_database}-rds-security-group"
-  description = "Security group for RDS"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "db_rds_security_group" {
+#   name        = "${var.rds_config.main_database}-rds-security-group"
+#   description = "Security group for RDS"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    description = "Home access"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = local.allowed_cidrs
-  }
+#   ingress {
+#     description = "Home access"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = local.allowed_cidrs
+#   }
 
-  ingress {
-    description = "VPC access"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = concat([module.vpc.vpc_cidr_block], module.vpc.vpc_secondary_cidr_blocks)
-  }
-}
+#   ingress {
+#     description = "VPC access"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = concat([module.vpc.vpc_cidr_block], module.vpc.vpc_secondary_cidr_blocks)
+#   }
+# }
 
-resource "aws_db_subnet_group" "postgresql_subnet_group" {
-  name       = "${var.rds_config.main_database}-db-subnet-group"
-  subnet_ids = var.rds_config.publicly_accessible ? module.vpc.public_subnets : module.vpc.private_subnets
-}
-
-
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
-
-  multi_az = true
+# resource "aws_db_subnet_group" "postgresql_subnet_group" {
+#   name       = "${var.rds_config.main_database}-db-subnet-group"
+#   subnet_ids = var.rds_config.publicly_accessible ? module.vpc.public_subnets : module.vpc.private_subnets
+# }
 
 
-  identifier = var.rds_config.main_database
+# module "db" {
+#   source = "terraform-aws-modules/rds/aws"
 
-  engine               = var.rds_config.engine.name
-  engine_version       = var.rds_config.engine.version
-  family               = "${var.rds_config.engine.name}${var.rds_config.engine.version}"
-  major_engine_version = var.rds_config.engine.version
-  instance_class       = var.rds_config.instance.class
-
-  allocated_storage     = var.rds_config.allocated_storage.initial
-  max_allocated_storage = var.rds_config.allocated_storage.max
+#   multi_az = true
 
 
-  db_name  = var.rds_config.main_database
-  username = var.rds_config.main_username
-  password = random_password.db_master_password.result
-  port     = 5432
+#   identifier = var.rds_config.main_database
 
-  skip_final_snapshot = var.rds_config.skip_final_snapshot
+#   engine               = var.rds_config.engine.name
+#   engine_version       = var.rds_config.engine.version
+#   family               = "${var.rds_config.engine.name}${var.rds_config.engine.version}"
+#   major_engine_version = var.rds_config.engine.version
+#   instance_class       = var.rds_config.instance.class
 
-  storage_encrypted = true
-  apply_immediately = true
+#   allocated_storage     = var.rds_config.allocated_storage.initial
+#   max_allocated_storage = var.rds_config.allocated_storage.max
 
-  vpc_security_group_ids = [aws_security_group.db_rds_security_group.id]
 
-  maintenance_window = var.rds_config.windows.maintenance
-  backup_window      = var.rds_config.windows.backup
+#   db_name  = var.rds_config.main_database
+#   username = var.rds_config.main_username
+#   password = random_password.db_master_password.result
+#   port     = 5432
 
-  performance_insights_enabled          = var.rds_config.performance_insights.enabled
-  performance_insights_retention_period = var.rds_config.performance_insights.retention_period
-  create_monitoring_role                = var.rds_config.monitoring_interval > 0
-  monitoring_interval                   = var.rds_config.monitoring_interval
-  monitoring_role_name                  = "${var.rds_config.main_database}-monitoring-role"
-  monitoring_role_use_name_prefix       = true
-  monitoring_role_description           = "Monitoring role for ${var.rds_config.main_database}"
+#   skip_final_snapshot = var.rds_config.skip_final_snapshot
 
-  parameters = var.rds_config.parameters
+#   storage_encrypted = true
+#   apply_immediately = true
 
-  db_subnet_group_name = aws_db_subnet_group.postgresql_subnet_group.name
-  # DB subnet group
-  subnet_ids          = var.rds_config.publicly_accessible ? module.vpc.public_subnets : module.vpc.private_subnets
-  publicly_accessible = var.rds_config.publicly_accessible
+#   vpc_security_group_ids = [aws_security_group.db_rds_security_group.id]
 
-  manage_master_user_password = random_password.db_master_password.result != ""
-}
+#   maintenance_window = var.rds_config.windows.maintenance
+#   backup_window      = var.rds_config.windows.backup
+
+#   performance_insights_enabled          = var.rds_config.performance_insights.enabled
+#   performance_insights_retention_period = var.rds_config.performance_insights.retention_period
+#   create_monitoring_role                = var.rds_config.monitoring_interval > 0
+#   monitoring_interval                   = var.rds_config.monitoring_interval
+#   monitoring_role_name                  = "${var.rds_config.main_database}-monitoring-role"
+#   monitoring_role_use_name_prefix       = true
+#   monitoring_role_description           = "Monitoring role for ${var.rds_config.main_database}"
+
+#   parameters = var.rds_config.parameters
+
+#   db_subnet_group_name = aws_db_subnet_group.postgresql_subnet_group.name
+#   # DB subnet group
+#   subnet_ids          = var.rds_config.publicly_accessible ? module.vpc.public_subnets : module.vpc.private_subnets
+#   publicly_accessible = var.rds_config.publicly_accessible
+
+#   manage_master_user_password = random_password.db_master_password.result != ""
+# }
--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,16 +1,16 @@
-module "rds_credentials" {
-  source      = "./modules/secretsmanager"
-  name        = "rds_credentials"
-  description = "RDS credentials"
-  secret_string = jsonencode(
-    {
-      "username" : "${module.db.db_instance_username}",
-      "password" : "${random_password.db_master_password.result}",
-      "engine" : "${var.rds_config.engine.name}",
-      "host" : "${module.db.db_instance_endpoint}",
-      "port" : "${module.db.db_instance_port}",
-      "dbInstanceIdentifier" : "${module.db.db_instance_identifier}"
-    }
-  )
-  recovery_window_in_days = 0
-}
+# module "rds_credentials" {
+#   source      = "./modules/secretsmanager"
+#   name        = "rds_credentials"
+#   description = "RDS credentials"
+#   secret_string = jsonencode(
+#     {
+#       "username" : "${module.db.db_instance_username}",
+#       "password" : "${random_password.db_master_password.result}",
+#       "engine" : "${var.rds_config.engine.name}",
+#       "host" : "${module.db.db_instance_address}",
+#       "port" : "${module.db.db_instance_port}",
+#       "dbInstanceIdentifier" : "${module.db.db_instance_identifier}"
+#     }
+#   )
+#   recovery_window_in_days = 0
+# }
