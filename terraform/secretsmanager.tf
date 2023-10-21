8116bc9330f28a25a444be57102f0ec52dcdac48
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-22 01:35:35 +0800

    Updated workflow

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -4,12 +4,12 @@ module "rds_credentials" {
   description = "RDS credentials"
   secret_string = jsonencode(
     {
-      "username" : "${module.db.cluster_master_username}",
-      "password" : "${module.db.cluster_master_password}",
+      "username" : "${module.db.db_instance_username}",
+      "password" : "${random_password.db_master_password.result}",
       "engine" : "${var.rds_config.engine.name}",
-      "host" : "${module.db.cluster_endpoint}",
-      "port" : "${module.db.cluster_port}",
-      "dbClusterIdentifier" : "${module.db.cluster_id}"
+      "host" : "${module.db.db_instance_endpoint}",
+      "port" : "${module.db.db_instance_port}",
+      "dbInstanceIdentifier" : "${module.db.db_instance_identifier}"
     }
   )
   recovery_window_in_days = 0
