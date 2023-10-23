e3c112eece92e3dba78167c17fac8631f221116b
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-23 16:03:27 +0800

    Updated the terraform resources names

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -63,7 +63,7 @@ resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-resource "local_file" "tf-key" {
+resource "local_file" "bastion_host_key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "${var.bastion_host_config.key_name}.pem"
 }
