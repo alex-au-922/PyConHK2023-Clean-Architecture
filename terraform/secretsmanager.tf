6f9248270f37081843a37cdf2c071e289a34eca2
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-24 00:30:29 +0800

    Removed the rds

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -1,20 +1,20 @@
-module "rds_credentials" {
-  source      = "./modules/secretsmanager"
-  name        = "${var.rds_config.main_database}_rds_credentials"
-  description = "RDS credentials"
-  secret_string = jsonencode(
-    {
-      "username" : "${module.db.db_instance_username}",
-      "password" : "${random_password.db_master_password.result}",
-      "engine" : "${var.rds_config.engine.name}",
-      "host" : "${module.db.db_instance_address}",
-      "port" : "${module.db.db_instance_port}",
-      "readerHost" : "${module.db_replica.db_instance_address}",
-      "readerPort" : "${module.db_replica.db_instance_port}",
-    }
-  )
-  recovery_window_in_days = 0
-}
+# module "rds_credentials" {
+#   source      = "./modules/secretsmanager"
+#   name        = "${var.rds_config.main_database}_rds_credentials"
+#   description = "RDS credentials"
+#   secret_string = jsonencode(
+#     {
+#       "username" : "${module.db.db_instance_username}",
+#       "password" : "${random_password.db_master_password.result}",
+#       "engine" : "${var.rds_config.engine.name}",
+#       "host" : "${module.db.db_instance_address}",
+#       "port" : "${module.db.db_instance_port}",
+#       "readerHost" : "${module.db_replica.db_instance_address}",
+#       "readerPort" : "${module.db_replica.db_instance_port}",
+#     }
+#   )
+#   recovery_window_in_days = 0
+# }
 
 module "opensearch_credentials" {
   source      = "./modules/secretsmanager"
