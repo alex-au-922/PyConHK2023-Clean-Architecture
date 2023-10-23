70d502f5444da9f3bcd44b5ebf655461fd01c4bb
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-23 21:53:26 +0800

    Check the directory of bastion keypair

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-# output "bastion_host_public_ipv4" {
-#   description = "Public IPv4 address of the Bastion Host"
-#   value       = aws_instance.bastion_host.public_ip
-# }
+output "bastion_host_public_ipv4" {
+  description = "Public IPv4 address of the Bastion Host"
+  value       = aws_instance.bastion_host.public_ip
+}
