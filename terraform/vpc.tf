3ec14b656b8522b3cab615dd90908b5ea6ed2f2c
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-23 22:12:30 +0800

    Delete bastion host

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -55,34 +55,34 @@ resource "aws_security_group" "bastion_host_security_group" {
   }
 }
 
-# resource "aws_key_pair" "bastion_host_key_pair" {
-#   key_name   = var.bastion_host_config.key_name
-#   public_key = tls_private_key.rsa.public_key_openssh
+resource "aws_key_pair" "bastion_host_key_pair" {
+  key_name   = var.bastion_host_config.key_name
+  public_key = tls_private_key.rsa.public_key_openssh
 
-#   provisioner "local-exec" {
-#     when    = create
-#     command = <<EOF
-#     echo "${tls_private_key.rsa.private_key_pem}" > ${var.bastion_host_config.key_name}.pem
-#     chmod 400 ${var.bastion_host_config.key_name}.pem
-#     EOF
-#   }
-# }
+  provisioner "local-exec" {
+    when    = create
+    command = <<EOF
+    echo "${tls_private_key.rsa.private_key_pem}" > ${var.bastion_host_config.key_name}.pem
+    chmod 400 ${var.bastion_host_config.key_name}.pem
+    EOF
+  }
+}
 
-# resource "tls_private_key" "rsa" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-# resource "aws_instance" "bastion_host" {
-#   ami                         = aws_ami_copy.encrypted_ami.id
-#   instance_type               = var.bastion_host_config.instance_type
-#   associate_public_ip_address = true
-#   vpc_security_group_ids = [
-#     aws_security_group.bastion_host_security_group.id,
-#     module.vpc.default_security_group_id,
-#   ]
+resource "aws_instance" "bastion_host" {
+  ami                         = aws_ami_copy.encrypted_ami.id
+  instance_type               = var.bastion_host_config.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.bastion_host_security_group.id,
+    module.vpc.default_security_group_id,
+  ]
 
-#   key_name  = aws_key_pair.bastion_host_key_pair.key_name
-#   subnet_id = module.vpc.public_subnets[0]
+  key_name  = aws_key_pair.bastion_host_key_pair.key_name
+  subnet_id = module.vpc.public_subnets[0]
 
-# }
+}
