ef8e79d25f17eaac593808fcd3593458db301e6e
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-30 04:02:52 +0800

    Updated the terraform

--- terraform/modules/ecr_compute_service_access/main.tf
+++ terraform/modules/ecr_compute_service_access/main.tf
@@ -41,25 +41,13 @@ data "aws_iam_policy_document" "this" {
     principals {
       type = "AWS"
       identifiers = [
-        data.aws_caller_identity.current.account_id
+        data.aws_caller_identity.current.account_id,
+        data.data.aws_caller_identity.current.arn,
       ]
     }
 
     actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:DeleteRepository",
-      "ecr:BatchDeleteImage",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
+      "ecr:*"
     ]
   }
   statement {
