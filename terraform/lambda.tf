2b65f3b1b3faeb6edadfb27f481bd5b33294f0eb
alexau <alexaupyconhk2023aws@gmail.com>
2023-10-30 05:28:19 +0800

    Updated terraform

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -93,7 +93,7 @@ module "data_embedding_handler_lambda" {
   reserved_concurrent_executions = var.lambda_config.data_embedding_handler.reserved_concurrent_executions
 
   create_package = false
-  image_uri      = "${module.data_embedding_handler_lambda.repository_url}:${data.aws_ecr_image.latest_data_embedding_handler_lambda_docker_image.image_tags[0]}"
+  image_uri      = "${module.data_embedding_handler_ecr.repository_url}:${data.aws_ecr_image.latest_data_embedding_handler_lambda_docker_image.image_tags[0]}"
   package_type   = var.lambda_config.data_embedding_handler.package_type
 
   environment_variables = {
@@ -133,7 +133,7 @@ module "data_embedding_handler_lambda" {
 
   event_source_mapping = {
     sqs = {
-      event_source_arn = module.updated_criteria_encoder_queue.queue_arn
+      event_source_arn = module.embedding_handler_queue.queue_arn
     }
   }
 
