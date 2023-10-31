d35283d366ad1edc88c5a53f3093bab5e95ae16b
alexau <alexaupyconhk2023aws@gmail.com>
2023-11-01 04:30:53 +0800

    Updated terraform to enable nat gateway

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,9 +4,10 @@ module "vpc" {
   name = var.vpc_config.name
   cidr = var.vpc_config.cidr_block
 
-  azs             = data.aws_availability_zones.available.names
-  private_subnets = var.vpc_config.private_subnets
-  public_subnets  = var.vpc_config.public_subnets
+  azs                = data.aws_availability_zones.available.names
+  private_subnets    = var.vpc_config.private_subnets
+  public_subnets     = var.vpc_config.public_subnets
+  enable_nat_gateway = true
 }
 
 data "aws_ami" "ami" {
