877ac6c831ab372664a0a44b0523dd75478f45dd
alexau <alexaupyconhk2023aws@gmail.com>
2023-11-05 21:48:49 +0800

    Updated the endpoint

--- sagemaker/endpoint.py
+++ sagemaker/endpoint.py
@@ -6,8 +6,18 @@ from contextlib import asynccontextmanager
 from onnxruntime import InferenceSession
 from transformers import AutoTokenizer
 import uvloop
+import logging
+import asyncio
+import numpy.typing as npt
+import numpy as np
 
 uvloop.install()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s | %(levelname)s] (%(name)s) >> %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 @asynccontextmanager
@@ -43,7 +53,19 @@ async def ping(request: Request) -> JSONResponse:
 
 @app.post("/invocations")
 def invocations(request: Request) -> JSONResponse:
+    request_body: dict = asyncio.run(request.json())
+    logging.info(request_body)
+
+    text = request_body["text"]
+
+    tokenized_text = app.state.tokenizer(
+        [text], return_tensors="np", padding=True, truncation=True
+    )
+    text_embedding: npt.NDArray[np.float_] = app.state.inference_session.run(
+        None, tokenized_text
+    )[0]
+
     return JSONResponse(
         status_code=200,
-        content={"message": "Hello World"},
+        content={"result": text_embedding.tolist()},
     )
