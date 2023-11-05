1af2e77c89d932a24c8b39378828825a12b3cfd7
alexau <alexaupyconhk2023aws@gmail.com>
2023-11-05 21:08:56 +0800

    Updated github workflow

---
+++ sagemaker/endpoint.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from typing import AsyncIterator
+from contextlib import asynccontextmanager
+from onnxruntime import InferenceSession
+from transformers import AutoTokenizer
+import uvloop
+
+uvloop.install()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    try:
+        app.state.inference_session = InferenceSession(
+            "./model", providers=["CPUExecutionProvider"]
+        )
+        app.state.tokenizer = AutoTokenizer.from_pretrained("./tokenizer")
+        yield
+    finally:
+        pass
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/ping")
+async def ping(request: Request) -> JSONResponse:
+    return JSONResponse(
+        status_code=200,
+        content={"message": "pong"},
+    )
+
+
+@app.post("/invocations")
+def invocations(request: Request) -> JSONResponse:
+    return JSONResponse(
+        status_code=200,
+        content={"message": "Hello World"},
+    )
--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,117 +1,117 @@
-data "aws_ecr_repository" "embedding_model" {
-  name = "${var.sagemaker_config.embedding_model.name}-sagemaker-model"
-}
+# data "aws_ecr_repository" "embedding_model" {
+#   name = "${var.sagemaker_config.embedding_model.name}-sagemaker-model"
+# }
 
-data "aws_ecr_image" "latest_embedding_model_docker_image" {
-  repository_name = data.aws_ecr_repository.embedding_model.name
-  most_recent     = true
-}
+# data "aws_ecr_image" "latest_embedding_model_docker_image" {
+#   repository_name = data.aws_ecr_repository.embedding_model.name
+#   most_recent     = true
+# }
 
-resource "aws_iam_role" "embedding_model" {
-  assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
-}
+# resource "aws_iam_role" "embedding_model" {
+#   assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
+# }
 
-data "aws_iam_policy_document" "sagemaker_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
+# data "aws_iam_policy_document" "sagemaker_assume_role" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["sagemaker.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["sagemaker.amazonaws.com"]
+#     }
+#   }
+# }
 
-data "aws_iam_policy_document" "embedding_model_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sagemaker:*"]
+# data "aws_iam_policy_document" "embedding_model_policy" {
+#   statement {
+#     effect  = "Allow"
+#     actions = ["sagemaker:*"]
 
-    resources = ["*"]
-  }
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:GetObject"]
-    resources = ["${module.sagemaker_model_bucket.s3_bucket_arn}/*"]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "cloudwatch:PutMetricData",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:CreateLogGroup",
-      "logs:DescribeLogStreams",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage"
-    ]
-    resources = ["*"]
-  }
-}
+#     resources = ["*"]
+#   }
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["s3:GetObject"]
+#     resources = ["${module.sagemaker_model_bucket.s3_bucket_arn}/*"]
+#   }
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "cloudwatch:PutMetricData",
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents",
+#       "logs:CreateLogGroup",
+#       "logs:DescribeLogStreams",
+#       "ecr:GetAuthorizationToken",
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:GetDownloadUrlForLayer",
+#       "ecr:BatchGetImage"
+#     ]
+#     resources = ["*"]
+#   }
+# }
 
-resource "aws_iam_policy" "embedding_model_policy" {
-  name   = "${var.sagemaker_config.embedding_model.name}-sagemaker-policy"
-  policy = data.aws_iam_policy_document.embedding_model_policy.json
-}
+# resource "aws_iam_policy" "embedding_model_policy" {
+#   name   = "${var.sagemaker_config.embedding_model.name}-sagemaker-policy"
+#   policy = data.aws_iam_policy_document.embedding_model_policy.json
+# }
 
-resource "aws_security_group" "embedding_model" {
-  name        = "${var.sagemaker_config.embedding_model.name}-sagemaker-security-group"
-  description = "Security group for SageMaker Embedding Model"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "embedding_model" {
+#   name        = "${var.sagemaker_config.embedding_model.name}-sagemaker-security-group"
+#   description = "Security group for SageMaker Embedding Model"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    description = "VPC access"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = concat([module.vpc.vpc_cidr_block], module.vpc.vpc_secondary_cidr_blocks)
-  }
+#   ingress {
+#     description = "VPC access"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = concat([module.vpc.vpc_cidr_block], module.vpc.vpc_secondary_cidr_blocks)
+#   }
 
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     description = "Allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-}
+# }
 
-resource "aws_iam_role_policy_attachment" "sagemaker" {
-  role       = aws_iam_role.embedding_model.arn
-  policy_arn = aws_iam_policy.embedding_model_policy.arn
-}
+# resource "aws_iam_role_policy_attachment" "sagemaker" {
+#   role       = aws_iam_role.embedding_model.arn
+#   policy_arn = aws_iam_policy.embedding_model_policy.arn
+# }
 
-resource "aws_sagemaker_model" "embedding_model" {
-  name               = var.sagemaker_config.embedding_model.name
-  execution_role_arn = aws_iam_role.embedding_model.arn
+# resource "aws_sagemaker_model" "embedding_model" {
+#   name               = var.sagemaker_config.embedding_model.name
+#   execution_role_arn = aws_iam_role.embedding_model.arn
 
-  primary_container {
-    image          = "${data.aws_ecr_repository.sagemaker_model.repository_url}:${data.aws_ecr_image.latest_sagemaker_model_docker_image.image_tags[0]}}"
-    model_data_url = "s3://${module.embedding_model_bucket.s3_bucket_id}/model.tar.gz"
-  }
+#   primary_container {
+#     image          = "${data.aws_ecr_repository.sagemaker_model.repository_url}:${data.aws_ecr_image.latest_sagemaker_model_docker_image.image_tags[0]}}"
+#     model_data_url = "s3://${module.embedding_model_bucket.s3_bucket_id}/model.tar.gz"
+#   }
 
-  vpc_config {
-    security_group_ids = [aws_security_group.embedding_model.id]
-    subnets            = module.vpc.private_subnets
-  }
-}
+#   vpc_config {
+#     security_group_ids = [aws_security_group.embedding_model.id]
+#     subnets            = module.vpc.private_subnets
+#   }
+# }
 
-resource "aws_sagemaker_endpoint_configuration" "embedding_model" {
-  name = "${var.sagemaker_config.embedding_model.name}-sagemaker-endpoint-configuration"
+# resource "aws_sagemaker_endpoint_configuration" "embedding_model" {
+#   name = "${var.sagemaker_config.embedding_model.name}-sagemaker-endpoint-configuration"
 
-  production_variants {
-    variant_name           = var.sagemaker_config.embedding_model.deployment.instance_variant.name
-    model_name             = aws_sagemaker_model.embedding_model.name
-    initial_instance_count = var.sagemaker_config.embedding_model.deployment.instance_count
-    instance_type          = var.sagemaker_config.embedding_model.deployment.instance_type
-    initial_variant_weight = var.sagemaker_config.embedding_model.deployment.instance_variant.weight
-  }
-}
+#   production_variants {
+#     variant_name           = var.sagemaker_config.embedding_model.deployment.instance_variant.name
+#     model_name             = aws_sagemaker_model.embedding_model.name
+#     initial_instance_count = var.sagemaker_config.embedding_model.deployment.instance_count
+#     instance_type          = var.sagemaker_config.embedding_model.deployment.instance_type
+#     initial_variant_weight = var.sagemaker_config.embedding_model.deployment.instance_variant.weight
+#   }
+# }
 
-resource "aws_sagemaker_endpoint" "embedding_model" {
-  name                 = "${var.sagemaker_config.embedding_model.name}-sagemaker-endpoint"
-  endpoint_config_name = aws_sagemaker_endpoint_configuration.embedding_model.name
-}
+# resource "aws_sagemaker_endpoint" "embedding_model" {
+#   name                 = "${var.sagemaker_config.embedding_model.name}-sagemaker-endpoint"
+#   endpoint_config_name = aws_sagemaker_endpoint_configuration.embedding_model.name
+# }
