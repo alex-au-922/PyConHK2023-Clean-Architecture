a242457b4139296bb5b1a9b1b4f083d8d1597d43
alexau <alexaupyconhk2023aws@gmail.com>
2023-11-05 23:32:09 +0800

    Updated the embedding lambda to use sagemaker endpoint

--- backend/data_embedding_handler/src/deployments/lambda/app.py
+++ backend/data_embedding_handler/src/deployments/lambda/app.py
@@ -5,7 +5,11 @@ from usecases import (
     UpsertEmbeddedProductDetailsUseCase,
     EmbedRawProductDetailsUseCase,
 )
-from adapters.embed_raw_product_details.onnx import OnnxEmbedRawProductDetailsClient
+
+# from adapters.embed_raw_product_details.onnx import OnnxEmbedRawProductDetailsClient
+from adapters.embed_raw_product_details.aws_sagemaker import (
+    AWSSageMakerEmbedRawProductDetailsClient,
+)
 from adapters.fetch_raw_product_details.postgres import (
     PostgresFetchRawProductDetailsClient,
 )
@@ -19,6 +23,7 @@ from onnxruntime import InferenceSession
 from transformers import AutoTokenizer
 from .config import (
     OnnxEmbedConfig,
+    AWSSageMakerEmbedConfig,
     PostgresConfig,
     OpenSearchConfig,
     ProjectConfig,
@@ -62,13 +67,19 @@ def init_embed_raw_product_details_client() -> None:
     if embed_raw_product_details_client is not None:
         return
 
-    inference_session = InferenceSession(
-        OnnxEmbedConfig.ONNX_MODEL_PATH, providers=["CPUExecutionProvider"]
-    )
-    tokenizer = AutoTokenizer.from_pretrained(OnnxEmbedConfig.TOKENIZER_PATH)
-    embed_raw_product_details_client = OnnxEmbedRawProductDetailsClient(
-        inference_session=inference_session,
-        tokenizer=tokenizer,
+    # inference_session = InferenceSession(
+    #     OnnxEmbedConfig.ONNX_MODEL_PATH, providers=["CPUExecutionProvider"]
+    # )
+    # tokenizer = AutoTokenizer.from_pretrained(OnnxEmbedConfig.TOKENIZER_PATH)
+    # embed_raw_product_details_client = OnnxEmbedRawProductDetailsClient(
+    #     inference_session=inference_session,
+    #     tokenizer=tokenizer,
+    # )
+
+    embed_raw_product_details_client = AWSSageMakerEmbedRawProductDetailsClient(
+        client_creator=lambda: boto3.client("sagemaker-runtime"),
+        endpoint_name=AWSSageMakerEmbedConfig.AWS_SAGEMAKER_ENDPOINT_NAME,
+        embed_batch_size=AWSSageMakerEmbedConfig.EMBED_BATCH_SIZE,
     )
 
 
