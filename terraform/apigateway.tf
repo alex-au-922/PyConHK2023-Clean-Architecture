e9595b1a9703be3653606ea496740adc8f76b5d7
alexau <alexaupyconhk2023aws@gmail.com>
2023-11-07 03:06:46 +0800

    Updated the terraform

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -32,67 +32,67 @@ module "api_gateway_lambda" {
   }
 }
 
-# resource "aws_cloudwatch_log_group" "api_gateway_ecs_logs" {
-#   name              = "/aws/api-gateway/${var.api_gateway_config.ecs.name}"
-#   retention_in_days = var.api_gateway_config.ecs.log_retention_days
-# }
+resource "aws_cloudwatch_log_group" "api_gateway_ecs_logs" {
+  name              = "/aws/api-gateway/${var.api_gateway_config.ecs.name}"
+  retention_in_days = var.api_gateway_config.ecs.log_retention_days
+}
 
 
-# module "api_gateway_ecs" {
-#   source = "terraform-aws-modules/apigateway-v2/aws"
+module "api_gateway_ecs" {
+  source = "terraform-aws-modules/apigateway-v2/aws"
 
-#   name          = var.api_gateway_config.ecs.name
-#   description   = var.api_gateway_config.ecs.description
-#   protocol_type = "HTTP"
+  name          = var.api_gateway_config.ecs.name
+  description   = var.api_gateway_config.ecs.description
+  protocol_type = "HTTP"
 
-#   cors_configuration = {
-#     allow_headers = ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key", "X-Amz-Security-Token"]
-#     allow_methods = ["POST", "GET", "OPTIONS"]
-#     allow_origins = ["*"]
-#   }
+  cors_configuration = {
+    allow_headers = ["Content-Type", "X-Amz-Date", "Authorization", "X-Api-Key", "X-Amz-Security-Token"]
+    allow_methods = ["POST", "GET", "OPTIONS"]
+    allow_origins = ["*"]
+  }
 
-#   create_api_domain_name = false
+  create_api_domain_name = false
 
-#   # Access logs
-#   default_stage_access_log_destination_arn = aws_cloudwatch_log_group.api_gateway_ecs_logs.arn
-#   default_stage_access_log_format          = jsonencode(var.api_gateway_config.ecs.access_log_format)
+  # Access logs
+  default_stage_access_log_destination_arn = aws_cloudwatch_log_group.api_gateway_ecs_logs.arn
+  default_stage_access_log_format          = jsonencode(var.api_gateway_config.ecs.access_log_format)
 
-#   integrations = {
-#     "${var.api_gateway_config.ecs.routes.query_handler.method} ${format("/%s/", join("/", var.api_gateway_config.ecs.routes.query_handler.path_parts))}" = {
-#       connection_type    = "VPC_LINK"
-#       vpc_link           = "my-vpc"
-#       integration_uri    = aws_lb.query_handler.arn
-#       integration_type   = "HTTP_PROXY"
-#       integration_method = "ANY"
-#     }
-#   }
+  integrations = {
+    "${var.api_gateway_config.ecs.routes.query_handler.method} ${format("/%s/", join("/", var.api_gateway_config.ecs.routes.query_handler.path_parts))}" = {
+      connection_type    = "VPC_LINK"
+      vpc_link           = "my-vpc"
+      integration_uri    = aws_lb.query_handler.arn
+      integration_type   = "HTTP_PROXY"
+      integration_method = "ANY"
+    }
+  }
 
-#    vpc_links = {
-#     my-vpc = {
-#       name               = "example"
-#       security_group_ids = [module.api_gateway_ecs_security_group.security_group_id]
-#       subnet_ids         = module.vpc.private_subnets
-#     }
-#   }
-# }
+  vpc_links = {
+    my-vpc = {
+      name               = "example"
+      security_group_ids = [module.api_gateway_ecs_security_group.security_group_id]
+      subnet_ids         = module.vpc.private_subnets
+    }
+  }
+}
 
-# module "api_gateway_ecs_security_group" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = "~> 4.0"
+module "api_gateway_ecs_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
 
-#   name        = "${var.api_gateway_config.ecs.name}-sg"
-#   description = "Security group for API Gateway ECS"
-#   vpc_id      = module.vpc.vpc_id
+  name        = "${var.api_gateway_config.ecs.name}-sg"
+  description = "Security group for API Gateway ECS"
+  vpc_id      = module.vpc.vpc_id
 
-#   ingress_with_cidr_blocks = [
-#     {
-#       from_port   = var.ecs_config.query_handler.container.port
-#       to_port     = var.ecs_config.query_handler.container.port
-#       description = "TCP from Everywhere"
-#       protocol    = "tcp"
-#       cidr_blocks = ["0.0.0.0/0"]
-#     }
-#   ]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = var.ecs_config.query_handler.container.port
+      to_port     = var.ecs_config.query_handler.container.port
+      description = "TCP from Everywhere"
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 
-#   egress_rules = ["all-all"]
-# }
+  egress_rules = ["all-all"]
+}
--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -29,221 +29,221 @@ resource "aws_security_group" "query_handler" {
   }
 }
 
-# resource "aws_lb" "query_handler" {
-#   name               = "${var.ecs_config.query_handler.name}-alb"
-#   load_balancer_type = "application"
-#   internal           = true
-#   security_groups    = [aws_security_group.query_handler.id]
-#   subnets            = module.vpc.public_subnets
-#   idle_timeout       = 300
-# }
+resource "aws_lb" "query_handler" {
+  name               = "${var.ecs_config.query_handler.name}-alb"
+  load_balancer_type = "application"
+  internal           = true
+  security_groups    = [aws_security_group.query_handler.id]
+  subnets            = module.vpc.public_subnets
+  idle_timeout       = 300
+}
 
-# resource "aws_lb_listener" "query_handler" {
-#   load_balancer_arn = aws_lb.query_handler.arn
-#   protocol          = "HTTP"
-#   port              = var.ecs_config.query_handler.container.port
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.query_handler.arn
-#   }
-# }
+resource "aws_lb_listener" "query_handler" {
+  load_balancer_arn = aws_lb.query_handler.arn
+  protocol          = "HTTP"
+  port              = var.ecs_config.query_handler.container.port
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.query_handler.arn
+  }
+}
 
-# resource "aws_lb_target_group" "query_handler" {
-#   name        = "${var.ecs_config.query_handler.name}-${substr(uuid(), 0, 3)}-lb"
-#   port        = var.ecs_config.query_handler.container.port
-#   protocol    = "HTTP"
-#   target_type = "ip"
-#   vpc_id      = module.vpc.vpc_id
-#   health_check {
-#     enabled             = true
-#     healthy_threshold   = var.ecs_config.query_handler.container.health_check.healthy_threshold
-#     unhealthy_threshold = var.ecs_config.query_handler.container.health_check.unhealthy_threshold
-#     interval            = var.ecs_config.query_handler.container.health_check.interval
-#     protocol            = "HTTP"
-#     matcher             = var.ecs_config.query_handler.container.health_check.matcher
-#     path                = format("/%s/", join("/", var.ecs_config.query_handler.container.health_check.path_parts))
-#   }
+resource "aws_lb_target_group" "query_handler" {
+  name        = "${var.ecs_config.query_handler.name}-${substr(uuid(), 0, 3)}-lb"
+  port        = var.ecs_config.query_handler.container.port
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+  health_check {
+    enabled             = true
+    healthy_threshold   = var.ecs_config.query_handler.container.health_check.healthy_threshold
+    unhealthy_threshold = var.ecs_config.query_handler.container.health_check.unhealthy_threshold
+    interval            = var.ecs_config.query_handler.container.health_check.interval
+    protocol            = "HTTP"
+    matcher             = var.ecs_config.query_handler.container.health_check.matcher
+    path                = format("/%s/", join("/", var.ecs_config.query_handler.container.health_check.path_parts))
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#     ignore_changes        = [name]
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [name]
+  }
+}
 
 
-# module "query_handler" {
-#   source = "terraform-aws-modules/ecs/aws"
+module "query_handler" {
+  source = "terraform-aws-modules/ecs/aws"
 
-#   cluster_name = var.ecs_config.query_handler.name
+  cluster_name = var.ecs_config.query_handler.name
 
-#   cluster_configuration = {
-#     execute_command_configuration = {
-#       logging = "OVERRIDE"
-#       log_configuration = {
-#         cloud_watch_log_group_name = var.ecs_config.query_handler.log_group.retention_days
-#       }
-#     }
-#   }
+  cluster_configuration = {
+    execute_command_configuration = {
+      logging = "OVERRIDE"
+      log_configuration = {
+        cloud_watch_log_group_name = var.ecs_config.query_handler.log_group.retention_days
+      }
+    }
+  }
 
-#   fargate_capacity_providers = {
-#     FARGATE = {
-#       default_capacity_provider_strategy = {
-#         weight = var.ecs_config.query_handler.fargate_capacity_config.normal_weight
-#       }
-#     }
-#     FARGATE_SPOT = {
-#       default_capacity_provider_strategy = {
-#         weight = var.ecs_config.query_handler.fargate_capacity_config.spot_weight
-#       }
-#     }
-#   }
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = var.ecs_config.query_handler.fargate_capacity_config.normal_weight
+      }
+    }
+    FARGATE_SPOT = {
+      default_capacity_provider_strategy = {
+        weight = var.ecs_config.query_handler.fargate_capacity_config.spot_weight
+      }
+    }
+  }
 
-#   services = {
-#     query_handler = {
-#       cpu                      = var.ecs_config.query_handler.container.cpu
-#       memory                   = var.ecs_config.query_handler.container.memory
-#       autoscaling_min_capacity = var.ecs_config.query_handler.container.autoscaling.min
-#       autoscaling_max_capacity = var.ecs_config.query_handler.container.autoscaling.max
+  services = {
+    query_handler = {
+      cpu                      = var.ecs_config.query_handler.container.cpu
+      memory                   = var.ecs_config.query_handler.container.memory
+      autoscaling_min_capacity = var.ecs_config.query_handler.container.autoscaling.min
+      autoscaling_max_capacity = var.ecs_config.query_handler.container.autoscaling.max
 
-#       # Container definition(s)
-#       container_definitions = {
+      # Container definition(s)
+      container_definitions = {
 
-#         (var.ecs_config.query_handler.container.name) = {
-#           cpu       = var.ecs_config.query_handler.container.cpu
-#           memory    = var.ecs_config.query_handler.container.memory
-#           essential = true
-#           image     = "${data.aws_ecr_repository.query_handler_ecr_ecs.repository_url}:${data.aws_ecr_image.latest_query_handler_ecs_docker_image.image_tags[0]}"
-#           port_mappings = [
-#             {
-#               name          = "http"
-#               containerPort = var.ecs_config.query_handler.container.port
-#               protocol      = "tcp"
-#             }
-#           ]
+        (var.ecs_config.query_handler.container.name) = {
+          cpu       = var.ecs_config.query_handler.container.cpu
+          memory    = var.ecs_config.query_handler.container.memory
+          essential = true
+          image     = "${data.aws_ecr_repository.query_handler_ecr_ecs.repository_url}:${data.aws_ecr_image.latest_query_handler_ecs_docker_image.image_tags[0]}"
+          port_mappings = [
+            {
+              name          = "http"
+              containerPort = var.ecs_config.query_handler.container.port
+              protocol      = "tcp"
+            }
+          ]
 
-#           environment = [
-#             {
-#               name  = "LOG_LEVEL",
-#               value = "INFO"
-#             },
-#             {
-#               name  = "LOG_FORMAT",
-#               value = "[{time:YYYY-MM-DD HH:mm:ss}|{level}] ({name}.py|{function}|line:{line}) >> {message}",
-#             },
-#             {
-#               name  = "LOG_JSON",
-#               value = "false"
-#             },
-#             {
-#               name  = "ONNX_MODEL_PATH",
-#               value = "model"
-#             },
-#             {
-#               name  = "TOKENIZER_PATH",
-#               value = "tokenizer"
-#             },
-#             {
-#               name  = "POSTGRES_SECRETS_MANAGER_NAME",
-#               value = module.rds_credentials.secretsmanager_secret_name
-#             },
-#             {
-#               name  = "POSTGRES_DB",
-#               value = var.rds_config.main_database
-#             },
-#             {
-#               name  = "POSTGRES_RAW_PRODUCT_TABLE_NAME",
-#               value = var.rds_table_config.raw_products.name
-#             },
-#             {
-#               name  = "POSTGRES_EMBEDDED_PRODUCT_TABLE_NAME",
-#               value = var.rds_table_config.embedded_products.name
-#             },
-#             {
-#               name  = "POSTGRES_FETCH_BATCH_SIZE",
-#               value = "1000"
-#             },
-#             {
-#               name  = "POSTGRES_UPSERT_BATCH_SIZE",
-#               value = "5"
-#             },
-#             {
-#               name  = "OPENSEARCH_SECRETS_MANAGER_NAME",
-#               value = module.opensearch_credentials.secretsmanager_secret_name
-#             },
-#             {
-#               name  = "OPENSEARCH_INDEX_NAME",
-#               value = var.opensearch_index_config.index.embedded_products.name
-#             },
-#             {
-#               name  = "OPENSEARCH_TIMEOUT",
-#               value = var.opensearch_index_config.timeout
-#             },
-#             {
-#               name  = "AWS_SQS_SUBSCRIBED_QUEUE_URL",
-#               value = module.embedding_handler_queue.queue_url
-#             },
-#             {
-#               name  = "SEARCH_DEFAULT_LIMIT",
-#               value = "10"
-#             },
-#             {
-#               name  = "SEARCH_DEFAULT_THRESHOLD",
-#               value = "0.3"
-#             },
-#             {
-#               name  = "TRANSFORMERS_CACHE",
-#               value = "/tmp"
-#             },
-#             {
-#               name  = "TZ",
-#               value = "${var.timezone}"
-#             }
-#           ]
+          environment = [
+            {
+              name  = "LOG_LEVEL",
+              value = "INFO"
+            },
+            {
+              name  = "LOG_FORMAT",
+              value = "[{time:YYYY-MM-DD HH:mm:ss}|{level}] ({name}.py|{function}|line:{line}) >> {message}",
+            },
+            {
+              name  = "LOG_JSON",
+              value = "false"
+            },
+            {
+              name  = "ONNX_MODEL_PATH",
+              value = "model"
+            },
+            {
+              name  = "TOKENIZER_PATH",
+              value = "tokenizer"
+            },
+            {
+              name  = "POSTGRES_SECRETS_MANAGER_NAME",
+              value = module.rds_credentials.secretsmanager_secret_name
+            },
+            {
+              name  = "POSTGRES_DB",
+              value = var.rds_config.main_database
+            },
+            {
+              name  = "POSTGRES_RAW_PRODUCT_TABLE_NAME",
+              value = var.rds_table_config.raw_products.name
+            },
+            {
+              name  = "POSTGRES_EMBEDDED_PRODUCT_TABLE_NAME",
+              value = var.rds_table_config.embedded_products.name
+            },
+            {
+              name  = "POSTGRES_FETCH_BATCH_SIZE",
+              value = "1000"
+            },
+            {
+              name  = "POSTGRES_UPSERT_BATCH_SIZE",
+              value = "5"
+            },
+            {
+              name  = "OPENSEARCH_SECRETS_MANAGER_NAME",
+              value = module.opensearch_credentials.secretsmanager_secret_name
+            },
+            {
+              name  = "OPENSEARCH_INDEX_NAME",
+              value = var.opensearch_index_config.index.embedded_products.name
+            },
+            {
+              name  = "OPENSEARCH_TIMEOUT",
+              value = var.opensearch_index_config.timeout
+            },
+            {
+              name  = "AWS_SQS_SUBSCRIBED_QUEUE_URL",
+              value = module.embedding_handler_queue.queue_url
+            },
+            {
+              name  = "SEARCH_DEFAULT_LIMIT",
+              value = "10"
+            },
+            {
+              name  = "SEARCH_DEFAULT_THRESHOLD",
+              value = "0.3"
+            },
+            {
+              name  = "TRANSFORMERS_CACHE",
+              value = "/tmp"
+            },
+            {
+              name  = "TZ",
+              value = "${var.timezone}"
+            }
+          ]
 
-#           command = var.ecs_config.query_handler.container.command
+          command = var.ecs_config.query_handler.container.command
 
-#           # Example image used requires access to write to root filesystem
-#           readonly_root_filesystem = !var.ecs_config.query_handler.container.filesystem_write_access
+          # Example image used requires access to write to root filesystem
+          readonly_root_filesystem = !var.ecs_config.query_handler.container.filesystem_write_access
 
-#           enable_cloudwatch_logging = true
-#           memory_reservation        = 100
-#         }
-#       }
+          enable_cloudwatch_logging = true
+          memory_reservation        = 100
+        }
+      }
 
-#       load_balancer = {
-#         service = {
-#           target_group_arn = aws_lb_target_group.query_handler.arn
-#           container_name   = var.ecs_config.query_handler.container.name
-#           container_port   = var.ecs_config.query_handler.container.port
-#         }
-#       }
+      load_balancer = {
+        service = {
+          target_group_arn = aws_lb_target_group.query_handler.arn
+          container_name   = var.ecs_config.query_handler.container.name
+          container_port   = var.ecs_config.query_handler.container.port
+        }
+      }
 
-#       tasks_iam_role_name = "${var.ecs_config.query_handler.name}-ecs"
-#       tasks_iam_role_statements = [
-#         {
-#           effect    = "Allow",
-#           actions   = ["secretsmanager:GetSecretValue"]
-#           resources = ["*"]
-#         }
-#       ]
+      tasks_iam_role_name = "${var.ecs_config.query_handler.name}-ecs"
+      tasks_iam_role_statements = [
+        {
+          effect    = "Allow",
+          actions   = ["secretsmanager:GetSecretValue"]
+          resources = ["*"]
+        }
+      ]
 
-#       subnet_ids = module.vpc.private_subnets
-#       security_group_rules = {
-#         alb_ingress = {
-#           type        = "ingress"
-#           from_port   = var.ecs_config.query_handler.container.port
-#           to_port     = var.ecs_config.query_handler.container.port
-#           protocol    = "tcp"
-#           cidr_blocks = [module.vpc.vpc_cidr_block]
-#         }
-#         egress_all = {
-#           type        = "egress"
-#           from_port   = 0
-#           to_port     = 0
-#           protocol    = "-1"
-#           cidr_blocks = ["0.0.0.0/0"]
-#         }
-#       }
-#     }
-#   }
-# }
+      subnet_ids = module.vpc.private_subnets
+      security_group_rules = {
+        alb_ingress = {
+          type        = "ingress"
+          from_port   = var.ecs_config.query_handler.container.port
+          to_port     = var.ecs_config.query_handler.container.port
+          protocol    = "tcp"
+          cidr_blocks = [module.vpc.vpc_cidr_block]
+        }
+        egress_all = {
+          type        = "egress"
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+  }
+}
